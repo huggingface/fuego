f03264b4b6c856d8c49600b385e48ed0298aadfb
nateraw <nxr9266@g.rit.edu>
2023-02-03 03:19:02 -0500

    :bug: add missing file

--- examples/simple_example/fuego_run.py
+++ examples/simple_example/fuego_run.py
@@ -1,8 +1,10 @@
+from pathlib import Path
+
 import fuego
 
 
 space_url, dataset_url = fuego.run(
-    "run.py",
+    script=str(Path(__file__).parent / "run.py"),
     delete_space_on_completion=True,  # When debugging, set this to False
     # Kwargs
     message="Howdy, world!!!",
